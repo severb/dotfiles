019e9414bedb9709eeec2bd308ff4f398357134c
Sever Băneşiu <banesiu.sever@gmail.com>
2011-12-05 10:02:16 +0200

    Updated bootstrap.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,22 +2,13 @@
 # To install the components run
 # wget -qO- http://bitbucket.org/severb/dotfiles/raw/tip/bootstrap.sh | /bin/bash
 
-echo 'Saving backup file in /tmp/dotfiles.tar.'
-tar cf /tmp/dotfiles.tar ~/.dotfiles/
+if [-d ~/.dotfiles ]; then
+    cd ~/.dotfiles
+    git pull
+else
+    git clone git://github.com/severb/dotfiles.git ~/.dotfiles/
+    for cf in ~/.dotfiles/configs/*; do
+        ln -s "$cf" ~/.$(basename "$cf")
+    done
+fi
 
-echo 'Deleting the old installment.'
-rm -rf ~/.dotfiles/
-
-hg clone https://severb@bitbucket.org/severb/dotfiles ~/.dotfiles/
-
-echo 'Creating configs links.'
-for cf in ~/.dotfiles/configs/*; do
-rm -rf ~/.$(basename "$cf")
-ln -s "$cf" ~/.$(basename "$cf")
-done
-
-# Create wiki script
-echo "#!/bin/bash" > /tmp/wikico
-echo hg clone https://severb@bitbucket.org/severb/wiki ~/.dotfiles/wiki >> /tmp/wikico
-chmod +x /tmp/wikico
-echo "(optional) Run /tmp/wikico to get the wiki."
